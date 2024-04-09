96bbc878873be195e6350e6d4d1ea2671682ac97
Kachimov Dmytro <137336892+Dima-Kachimov@users.noreply.github.com>
2024-04-09 23:32:34 +0300

    add: conditions.js, jsCommand.js

---
+++ JavaScript/conditions.js
@@ -0,0 +1,21 @@
+// Условия
+
+/* ***Условия ***
+*  1) if (a > b) {}
+*  2) if (a > b) {} else {}
+*  3) if (a > b) {} else if (a < b) {} else {}
+*  4) 1 > 2 ? 'true' : 'false' - тернарный оператор
+* */
+
+// *** Конструкция switch case ***
+// switch (1 + 3) {
+//     case 1:
+//         console.log('если условие с case совпадают, выполниться этот код')
+//         break;
+//     case 4:
+//         console.log('4')
+//         break
+//     default:
+//         console.log('default')
+//         break;
+// }
