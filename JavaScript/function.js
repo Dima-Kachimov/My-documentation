35ea9a2692b36e2e47a5032cbc1929c3d8171e0b
Kachimov Dmytro <137336892+Dima-Kachimov@users.noreply.github.com>
2024-04-12 20:19:22 +0300

    add: function.js

---
+++ JavaScript/function.js
@@ -0,0 +1,15 @@
+// Function
+// - Если нет return вернеться undefined
+
+// Declaration function
+// - + hosting
+// - можно вызвать до ее обьявления
+function decFn() {}
+
+// Function expression - функионнальное вырожение
+// - можно вызвать только после ее обьявления
+const expFn = function () {}
+
+// Arrow function - стрелосчная функция
+// - не имеет своего контекста (this)
+const arrowFn = () => {}
